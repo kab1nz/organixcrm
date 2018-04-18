3112d1d0750fc8e16213d3b467d4b7a0bd95b219
rgm19 <gmruben1995@gmail.com>
2018-04-18 11:57:57 +0200

    cambios varios

--- bd/conexion.php
+++ bd/conexion.php
@@ -3,6 +3,6 @@
     if(!$conexion){
         echo "Error al conectar a la base de datos";
     }else{
-        echo "Conectado a la base de datos";
+     //echo "Conectado a la base de datos";
     }
 ?>
